3b1d04705dabf1979242c941ff797d84eb199b76
Omar Zarate <alberich_sid@hotmail.com>
2014-08-24 13:56:54 -0500

    se creo toda la interfaz grafica de pagina

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+  	@nombrePeli= "Los vengadores de Marvel"
+  	@imagen = "omar.JPG"
+  	@link = "https://www.mega.coz/jhdjhdj"
+  end
+  def batman
+  	@nombrePeli= "Batman el Caballero de la Noche"
+  	@imagen = "batman.jpg"
+  	@link = "https://www.mega.coz/batamn-hsdfh/sdjs"
+  end
+  def superman
+  	@nombrePeli= "Superman el Hombre de Acero"
+  	@imagen = "super.jpg"
+  	@link = "https://www.mega.coz/super-hsdfh/sdjs"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/batman'
+  get 'welcome/superman'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/batman' => 'welcome#batman', as: :batman
+  get '/superman' => 'welcome#superman', as: :superman
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
